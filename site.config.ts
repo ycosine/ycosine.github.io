090ee44f8ded56824bcc588b11ea8ad9b22c4e4b
yuxianjun <yuxianjun@gmail.com>
2023-01-05 17:38:31 +0800

    feat: publish

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '余弦是定理',
+  domain: 'ycosine.xyz',
+  author: 'yuxianjun',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
